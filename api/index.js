92946ce71143e62d98542b8c70107735c33558c7
trunghau-cauhai <trunghau123@gmail.com>
2025-09-06 20:26:56 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
 // GET https://<project>.vercel.app/api
 module.exports = (req, res) => {
-  res.status(200).send("CAHA Webhook is running (Vercel Functions)");
+  res.status(200).send("CAUHAI Webhook is running (Vercel Functions)");
 };
